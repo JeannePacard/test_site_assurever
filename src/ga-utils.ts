98f8457f0664c1303be1acfdf1e233ca08f7e2c4
JeannePacard <jeanne.pacard@gmail.com>
2023-08-22 11:53:17 +0200

    add google analytics ID

--- src/ga-utils.ts
+++ src/ga-utils.ts
@@ -1,7 +1,7 @@
 import * as ReactGA from "react-ga";
 
 export const initGA = () => {
-  const GOOGLE_ANALYTICS_ID = "#";
+  const GOOGLE_ANALYTICS_ID = "G-YJPX0GBTJ3";
   if (process.env.NODE_ENV === "production") {
     ReactGA.initialize(GOOGLE_ANALYTICS_ID);
   }
